80aecb14573bf69a2bc1a5f761d65adc80967d87
gmldud76 <90237101+gmldud76@users.noreply.github.com>
2023-12-17 21:17:10 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -153,15 +153,16 @@ def show_species_data():
 def main():
     st.sidebar.title('못난이 삼남매')
     #app_mode = st.sidebar.radio("분석 페이지를 골라주세요.", ["입지 분석", "감성 분석"])
+    appmode = st.sidebar.radio("분석 페이지를 골라주세요.", ["입지 분석", "감성 분석"])
     with st.sidebar:
-        choice = option_menu("분석 페이지를 골라주세요.",["입지 분석", "감성 분석"],
+        choice = appmode_menu("분석 페이지를 골라주세요.",["입지 분석", "감성 분석"],
                                icons = ['bar-chart', 'kanban'],
                                menu_icon = "bi bi-menu-up", default_index=0,
                                styles={
                 "icon":{"color":"orange","font-size":"25px"}})
-    if app_mode == "입지 분석":
+    if appmode == "입지 분석":
         show_home()
-    elif app_mode == "감성 분석":
+    elif appmode == "감성 분석":
         show_species_data()
     
 if __name__ == "__main__":
